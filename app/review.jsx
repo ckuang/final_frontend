4fb33b2a11982cb7e897fda7c0b6c0a673338893
Ambar Gutierrez <ambar.grz@gmaail.com>
2017-01-19 17:58:46 -0500

    front-end final exam

--- app/review.jsx
+++ app/review.jsx
@@ -4,17 +4,34 @@ var $ = require('jquery')
 var Review = React.createClass({
   getInitialState: function (){
     return {
-
+      date: '',
+      description: '',
+      rating: '1'
     }
   },
+  handleChange(key, event){
+    this.setState({[key]: event.target.value})
+  },
+  handleSubmit(event){
+    event.preventDefault();
+    const {date, description, rating} = this.state
+    $.ajax({
+      url: '/api/review',
+      type: 'POST',
+      data: {date, description, rating}
+    })
+  },
+
+
   render: function() {
+console.log(this.state)
     return (
       <div>
-        <form>
-          <input type="date"/> <br/>
-          <textarea type="text" placeholder="Describe your experience"/> <br/>
-          <select>
-            <option value="1">*</option>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange.bind(this, 'date')}  value={this.state.date}type="date"/> <br/>
+          <textarea onChange={this.handleChange.bind(this, 'description')} value={this.state.description} type="text" placeholder="Describe your experience"/> <br/>
+          <select onChange={this.handleChange.bind(this, 'rating')} >
+            <option value="1" default>*</option>
             <option value="2">**</option>
             <option value="3">***</option>
             <option value="4">****</option>
