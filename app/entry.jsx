2ec2aee4a2879624bd1b8d99181ae437aba30839
Dee Sterling <darlenesterling@gmail.com>
2017-02-22 18:00:20 -0500

    Completing Test

--- app/entry.jsx
+++ app/entry.jsx
@@ -15,6 +15,14 @@ let App = React.createClass({
   }
 })
 
-ReactDOM.render(
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component= {Restaurants} />
+      <Route path="/restaurants" component={Restaurants} />
+      <Route path="/restaurant/:id" component={Restaurants} />
 
-)
+
+    </Route>
+  </Router>
+), document.getElementById('root'));
